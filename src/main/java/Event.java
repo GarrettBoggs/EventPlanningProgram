301674866513175bc32f6f420c701cb73d6c94ed
garrett.boggs@my.wheaton.edu <Guest@epicodus-21.local>
2016-09-09 09:03:25 -0700

    create a test for a band

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,19 +1,27 @@
 class Event {
   private boolean mChips;
   private int mPeople;
+  private boolean mDrinks;
+  private String mBand;
 
-  public Event(boolean chips, int people) {
+  public Event(boolean chips, boolean drinks, int people, String band) {
     mChips = chips;
+    mDrinks = drinks;
     mPeople = people;
+    mBand = band;
   }
 
   public int getCost() {
     int total = 0;
-
     if(mChips){
       total += 5;
     }
-
+    if(mDrinks){
+      total += 10;
+    }
+    if(mBand == "weezer"){
+      total += 40;
+    }
     total += mPeople;
     return total;
   }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -4,31 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EventTest{
-
   @Test
   public void newEvent_instantiatesCorrectly() {
-    Event testEvent = new Event(true,5);
+    Event testEvent = new Event(true,false,5,"none");
     assertEquals(true, testEvent instanceof Event);
   }
 
   @Test
-  public void getCost_CostreturnsMoney_0() {
-    Event testEvent = new Event(true,5);
+  public void getCost_costReturnsMoney_0() {
+    Event testEvent = new Event(true,false,5,"none");
     Integer convertThis = new Integer(testEvent.getCost());
     assertEquals(true, convertThis instanceof Integer);
   }
 
   @Test
-  public void getCost_costIncreaseswithChips_5() {
-    Event testEvent = new Event(true,0);
+  public void getCost_costIncreasesWithChips_5() {
+    Event testEvent = new Event(true,false,0,"none");
     assertEquals(5, testEvent.getCost());
   }
 
   @Test
-  public void getCost_costIncreaseswithPeople_10() {
-    Event testEvent = new Event(true,5);
+  public void getCost_costIncreasesWithPeople_10() {
+    Event testEvent = new Event(true,false,5,"none");
     assertEquals(10, testEvent.getCost());
   }
 
+  @Test
+  public void getCost_costIncreasesWithDrinks_20() {
+    Event testEvent = new Event(true,true,5,"none");
+    assertEquals(20, testEvent.getCost());
+  }
 
+  @Test
+  public void getCost_costIncreasesWithBand_20() {
+    Event testEvent = new Event(true,true,5,"weezer");
+    assertEquals(60, testEvent.getCost());
+  }
 }
